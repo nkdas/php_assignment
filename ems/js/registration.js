a800708580d5b0785805cc006bcc332088d286a5
nkdas <neeraj.das@mindfiresolutions.com>
2016-01-27 17:50:27 +0530

    Update registration.js

--- ems/js/registration.js
+++ ems/js/registration.js
@@ -33,115 +33,126 @@ function readURL(input) {
 }
 
 function validate(origin) {
-	var errors = "";
+	var errors = "<br><label class='myLabel'>Please Fix the following errors:</label><br>";
 	var letters = /^[a-zA-Z ]+$/;
 	var numbers = /^[0-9]+$/;
 	var emailRegex = /^[a-z0-9_-]+@[a-z0-9._-]+\.[a-z]+$/;
 
+	var photo = document.getElementById("uploadBtn").value;
+	var extension = photo.substring(photo.lastIndexOf('.') + 1);
+	if(extension == "JPEG" || extension == "jpeg" || extension == "jpg" || extension == "JPG" || 
+		extension == "png" || extension == "PNG" || extension == "gif" || extension == "GIF"){	
+	}
+	else{
+		errors += "<label class='myLabel'>Only JPG, PNG and GIF images are allowed as profile photo</label><br>";
+	}
+
 	if (origin == "register") {
 		if (document.getElementById('username').value == "") {
-			errors += "Username cannot be blank\n";
+			errors += "<label class='myLabel'>Username cannot be blank</label><br>";
 		}
 		else if (document.getElementById('username').value.length < 6) {
-			errors += "Username should be of atleast 6 characters\n";
+			errors += "<label class='myLabel'>Username should be of atleast 6 characters</label><br>";
 		}
 
 		if (document.getElementById('password').value == "") {
-			errors += "Password cannot be blank\n";
+			errors += "<label class='myLabel'>Password cannot be blank</label><br>";
 		}
 		else if (document.getElementById('password').value.length < 6) {
-			errors += "Password should be of atleast 6 characters\n";
+			errors += "<label class='myLabel'>Password should be of atleast 6 characters</label><br>";
 		}
 
 		if (document.getElementById('rpassword').value == "") {
-			errors += "Please reenter your password\n";
+			errors += "<label class='myLabel'>Please re-enter your password</label><br>";
 		}
 		else if (document.getElementById('password').value != document.getElementById('rpassword').value) {
-			errors += "Passwords entered in the 'Password' and 'Re-enter Password' fields donot match\n";
+			errors += "<label class='myLabel'>Passwords entered in the 'Password' and 'Re-enter Password' fields donot match</label><br>";
 		}
 	}
 
 	if (document.getElementById('firstname').value == "") {
-		errors += "Firstname cannot be blank\n";
+		errors += "<label class='myLabel'>Firstname cannot be blank</label><br>";
 	}
 	else if (!document.getElementById('firstname').value.match(letters)) {
-		errors += "Firstname should contain only characters\n";
+		errors += "<label class='myLabel'>Firstname should contain only characters</label><br>";
 	}
 
 	if (document.getElementById('lastname').value == "") {
-		errors += "Lastname cannot be blank\n";
+		errors += "<label class='myLabel'>Lastname cannot be blank</label><br>";
 	}
 	else if (!document.getElementById('lastname').value.match(letters)) {
-		errors += "Lastname should contain only characters\n";
+		errors += "<label class='myLabel'>Lastname should contain only characters</label><br>";
 	}
 
 	if (document.getElementById('suffixSelector').value == "") {
-		errors += "Please select a suffix\n";
+		errors += "<label class='myLabel'>Please select a suffix</label><br>";
 	}
 	if (document.getElementById('datePicker').value == "") {
-		errors += "Please enter your date of birth\n";
+		errors += "<label class='myLabel'>Please enter your date of birth</label><br>";
 	}
 	if (document.getElementById('employementSelector').value == "") {
-		errors += "Please select Employement\n";
+		errors += "<label class='myLabel'>Please select Employement</label><br>";
 	}
 
 	if (document.getElementById('employer').value == "") {
-		errors += "Employer cannot be blank\n";
+		errors += "<label class='myLabel'>Employer cannot be blank</label><br>";
 	}
 	else if (!document.getElementById('employer').value.match(letters)) {
-		errors += "Employer should contain only characters\n";
+		errors += "<label class='myLabel'>Employer should contain only characters</label><br>";
 	}
 
 	if (document.getElementById('email').value == "") {
-		errors += "EMail ID cannot be blank\n";
+		errors += "<label class='myLabel'>EMail ID cannot be blank</label><br>";
 	}
 	else if (!document.getElementById('email').value.match(emailRegex)) {
-		errors += "Please enter a valid email\n";
+		errors += "<label class='myLabel'>Please enter a valid email</label><br>";
 	}
 
 	if ( (document.getElementById('male').checked == false) && (document.getElementById('female').checked == false) ) {
-		errors += "Please select a gender\n";
+		errors += "<label class='myLabel'>Please select a gender</label><br>";
 	}
 	if (document.getElementById('street').value == "") {
-		errors += "Please enter your residential street\n";
+		errors += "<label class='myLabel'>Please enter your residential street</label><br>";
 	}
 	if (document.getElementById('city').value == "") {
-		errors += "Please enter your residential city\n";
+		errors += "<label class='myLabel'>Please enter your residential city</label><br>";
 	}
 	else if (!document.getElementById('city').value.match(letters)) {
-		errors += "City should contain only characters\n";
+		errors += "<label class='myLabel'>City should contain only characters</label><br>";
 	}
 
 	if (document.getElementById('state').value == "") {
-		errors += "Please enter your residential state\n";
+		errors += "<label class='myLabel'>Please enter your residential state</label><br>";
 	}
 	else if (!document.getElementById('state').value.match(letters)) {
-		errors += "State should contain only characters\n";
+		errors += "<label class='myLabel'>State should contain only characters</label><br>";
 	}
 
 	if (document.getElementById('zip').value == "") {
-		errors += "Please enter your residential zip code\n";
+		errors += "<label class='myLabel'>Please enter your residential zip code</label><br>";
 	}
 
 	if (document.getElementById('telephone').value == "") {
-		errors += "Please enter your residential telephone number\n";
+		errors += "<label class='myLabel'>Please enter your residential telephone number</label><br>";
 	}
 	else if (!document.getElementById('telephone').value.match(numbers)) {
-		errors += "Telephone number should contain only numbers\n";
+		errors += "<label class='myLabel'>Telephone number should contain only numbers</label><br>";
 	}
 
 	if (document.getElementById('mobile').value == "") {
-		errors += "Please enter your residential mobile number\n";
+		errors += "<label class='myLabel'>Please enter your residential mobile number</label><br>";
 	}
 	else if (!document.getElementById('mobile').value.match(numbers)) {
-		errors += "Mobile number should contain only numbers\n";
+		errors += "<label class='myLabel'>Mobile number should contain only numbers</label><br>";
 	}
 	else if ( (document.getElementById('mobile').value.length < 10) || (document.getElementById('mobile').value.length > 10) ) {
-		errors += "Mobile number should be of 10 digits\n";
+		errors += "<label class='myLabel'>Mobile number should be of 10 digits</label><br>";
 	}
 
 	if (errors.length != 0) {
-		alert(errors);
+		document.getElementById('message').style.display = "block";
+		document.getElementById('message').innerHTML = errors;
+		window.scroll(0,0);
 		return false;
 	}
 }
